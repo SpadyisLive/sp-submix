02ed192bb47c854e6e72fab0b0156a66af9fbc34
SpadyisLive <82928918+SpadyisLive@users.noreply.github.com>
2023-12-05 10:41:23 +0545

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,50 @@
+function EnableSubmix()
+    radioEffectId = CreateAudioSubmix('Radio')
+    SetAudioSubmixEffectRadioFx(radioEffectId, 0)
+    SetAudioSubmixEffectRadioFx(0, 0)
+    SetAudioSubmixEffectParamInt(0, 0, `default`, 1)
+    SetAudioSubmixEffectParamFloat(0, 0, `freq_low`, 0.0)
+    SetAudioSubmixEffectParamFloat(0, 0, `freq_hi`, 10000.0)
+    SetAudioSubmixEffectParamFloat(0, 0, `fudge`, 0.0)
+    SetAudioSubmixEffectParamFloat(0, 0, `rm_mix`, 0.2)
+end
+
+function DisableSubmix()
+    SetAudioSubmixEffectRadioFx(0, 0)
+    SetAudioSubmixEffectParamInt(0, 0, `enabled`, 0)
+end
+
+
+local soundmix = false
+CreateThread(function()
+    while true do
+        Wait(5000)
+        ped = PlayerPedId()
+        currentVehicle = GetVehiclePedIsIn(ped, false)
+        local vehmodel = GetEntityModel(currentVehicle)
+        if IsThisModelAHeli(vehmodel) or IsThisModelAPlane(vehmodel) then
+            if IsPedInAnyVehicle(ped, false) then
+                if GetIsVehicleEngineRunning(currentVehicle) then
+                    if soundmix == false then
+                        EnableSubmix()
+                        soundmix = true
+                    end
+                else
+                    if soundmix == true then 
+                        DisableSubmix()
+                        soundmix = false
+                    end        
+                end 
+            else   
+                if soundmix == true then 
+                    DisableSubmix() 
+                    soundmix = false 
+                end                
+            end
+        end
+        if not IsPedInAnyVehicle(ped, false) then
+            DisableSubmix()
+            soundmix = false
+        end   
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+author 'Spady'
+description 'Submix Sound effect like NoPixel for Helicopter and plane'
+version '1.0.0'
+client_script 'client.lua'
